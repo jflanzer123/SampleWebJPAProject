31a4902143d24a973e32a4d71813c0a7ecae36c4
jflanzer123 <jflanzer123@gmail.com>
2018-01-22 13:51:34 -0500

    commit

--- src/main/java/com/jflanzer/springboot/web/jpa/customerInvoices/repository/InvoiceLineRepository.java
+++ src/main/java/com/jflanzer/springboot/web/jpa/customerInvoices/repository/InvoiceLineRepository.java
@@ -1,5 +1,11 @@
 package com.jflanzer.springboot.web.jpa.customerInvoices.repository;
 
-public class InvoiceLineRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jflanzer.springboot.web.jpa.customerInvoices.entities.InvoiceLine;
+
+//InvoiceLine interface for spring data jpa entity management
+public interface InvoiceLineRepository extends JpaRepository<InvoiceLine,
+	Integer>{
 
 }
